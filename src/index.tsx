d1991b0ef42aba1cbf973e6b60f99a5705b742ef
cookerolo <cookerolo@yahoo.com.ar>
2021-05-13 14:47:29 -0300

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,89 @@
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const ref = useRef<any>();
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm'
+      // 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm' => alternative to saving the file on the project
     });
-    console.log(service);
   };
 
   useEffect(() => {
     startService();
   }, []);
 
-  const onClick = () => {
-    setCode(input);
+  const onClick = async () => {
+    setLoading(true);
+    if (!ref.current) {
+      return;
+    }
+    try {
+      const result = await ref.current.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+        define: {
+          'process.env.NODE_ENV': '"production"',
+          global: 'window'
+        }
+      });
+      setCode(result.outputFiles[0].text);
+      setInput('');
+    } catch (err) {
+      setCode(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onClickClear = () => {
     setInput('');
+    setCode('');
   };
 
   return (
     <div>
-      <h1>Code Transpiler</h1>
       <textarea
         className='textarea'
         placeholder='Insert your code here!'
-        rows={8}
+        rows={10}
         value={input}
         onChange={(e) => setInput(e.target.value)}
       ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
+      <br />
+      <div className='buttons'>
+        <button
+          className='button is-primary is-outlined is-primary is-rounded'
+          disabled={!input}
+          onClick={onClick}
+        >
+          Submit
+        </button>
+        <button
+          className='button is-primary is-outlined is-danger is-rounded'
+          disabled={!code && !input}
+          onClick={onClickClear}
+        >
+          Clear
+        </button>
       </div>
-      {code ? (
-        <pre>{code}</pre>
-      ) : (
-        <pre>Your transpiled code should appear here once you submit it!</pre>
+      {loading && (
+        <progress className='progress is-small is-primary' max='100'>
+          45%
+        </progress>
       )}
+      {code ? <pre>{code}</pre> : <pre>Your Transpiled code should appear here!</pre>}
     </div>
   );
 };
